12264274304a8b4d3dcdf0aac902869bb89a8086
Faxiang Li <lifa0917@gmail.com>
2019-06-26 21:46:13 -0400

    Add get posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,4 +44,17 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const post = await Post.find().sort({ date: -1 });
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
